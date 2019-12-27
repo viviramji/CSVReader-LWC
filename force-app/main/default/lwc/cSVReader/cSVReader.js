70447b7926a9645d077f44b4400cea3a06f39d72
viviramji <viviramji@gmail.com>
2019-12-27 13:59:38 -0500

    brig data from files and show into a lightning table

--- force-app/main/default/lwc/cSVReader/cSVReader.js
+++ force-app/main/default/lwc/cSVReader/cSVReader.js
@@ -4,6 +4,7 @@ import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 import readCSV from '@salesforce/apex/CSVReaderController.readCSVFile';
 
 const columns = [
+    { label: 'id', fieldName: 'Aux_Id__c'},
     { label: 'Identity Document', fieldName: 'Identity_Document__c' }, 
     { label: 'First Name', fieldName: 'First_Name__c' },
     { label: 'Last Name', fieldName: 'Last_Name__c'},
@@ -29,25 +30,31 @@ export default class CSVReader extends LightningElement {
         readCSV({idContentDocument : uploadedFiles[0].documentId})
         .then(result => {
             window.console.log('result ===> '+result);
+            this.error = '';
             this.data = result;
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Success!!',
-                    message: 'Accounts are created based CSV file!!!',
+                    message: 'Uploaded file!',
                     variant: 'success',
                 }),
             );
         })
         .catch(error => {
-            this.error = error;
+            var error_excep = JSON.parse(JSON.stringify(error)).body;
+            this.error = error_excep.message;
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error!!',
-                    message: JSON.stringify(error),
+                    message: error,
                     variant: 'error',
                 }),
             );     
         })
+    }
 
+    importData(){
+        // TODO: Import process
+        window.console.log(this.data.Last_Name__);
     }
 }
