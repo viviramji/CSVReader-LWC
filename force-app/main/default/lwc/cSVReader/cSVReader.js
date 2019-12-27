1d2c7680ad54114e5f5afc24da0110c0c08a7f7e
viviramji <viviramji@gmail.com>
2019-12-27 01:39:52 -0500

    Saved files, and show into a list, TODO: check when it's ',' or ';', dont insert account before is and other stuff

---
+++ force-app/main/default/lwc/cSVReader/cSVReader.js
@@ -0,0 +1,54 @@
+import { LightningElement, track, api } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+import readCSV from '@salesforce/apex/CSVReaderController.readCSVFile';
+
+const columns = [
+    { label: 'Name', fieldName: 'Name' }, 
+    { label: 'Industry', fieldName: 'Industry' },
+    { label: 'Rating', fieldName: 'Rating'}, 
+    { label: 'Type', fieldName: 'Type'}, 
+    { label: 'Website', fieldName: 'Website', type:'url'}
+];
+
+export default class CSVReader extends LightningElement {
+    @api recordId;
+    @track error;
+    @track columns = columns;
+    @track data;
+
+    // accepted parameters
+    get acceptedFormats() {
+        return ['.csv'];
+    }
+    
+    handleUploadFinished(event) {
+        // Get the list of uploaded files
+        const uploadedFiles = event.detail.files;
+
+        // calling apex class
+        readCSV({idContentDocument : uploadedFiles[0].documentId})
+        .then(result => {
+            window.console.log('result ===> '+result);
+            this.data = result;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success!!',
+                    message: 'Accounts are created based CSV file!!!',
+                    variant: 'success',
+                }),
+            );
+        })
+        .catch(error => {
+            this.error = error;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error!!',
+                    message: JSON.stringify(error),
+                    variant: 'error',
+                }),
+            );     
+        })
+
+    }
+}
